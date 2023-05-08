cdc116b86adc9029bcabe3a20757af33fb7b63bc
Dusan <shandoo91@gmail.com>
2023-05-08 22:29:47 +0200

    alert to console.log

--- popup/popup.js
+++ popup/popup.js
@@ -1,5 +1,5 @@
 // get selected text from local storage
 chrome.storage.local.get('selectedText', (text) => {
   const selectedText = text.selectedText;
-  window.alert(selectedText);
+  console.log(selectedText);
 });
