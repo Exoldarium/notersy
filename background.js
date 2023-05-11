5dd598c7ddeb220871752b5d92aa2870e42208ad
Dusan <shandoo91@gmail.com>
2023-05-11 19:21:33 +0200

    removed data variable and logs
    realized the data variable is unnecessary and i can just push the data straight to local storage and there's no need to shift anything

--- background.js
+++ background.js
@@ -10,23 +10,19 @@ chrome.runtime.onInstalled.addListener(() => {
 (async () => {
   // grab data from local storage
   const res = await chrome.storage.local.get('selectedText');
-  const data = res.selectedText;
-  data.shift();
 
   // update local storage with new data from popup.js
   chrome.runtime.onMessage.addListener((request) => {
-    console.log(request.message);
-    data.length = 0;
-    data.push(...request.message);
+    res.selectedText.length = 0; // set storage array to 0 so that it doesn't duplicate all the data when new data is pushed
+    res.selectedText.push(...request.message);
   });
 
   // add new notes on context menu click
   chrome.contextMenus.onClicked.addListener((text) => {
-    data.push({
+    res.selectedText.push({
       url: text.pageUrl,
       text: text.selectionText,
     });
-    chrome.storage.local.set({ "selectedText": data });
+    chrome.storage.local.set({ "selectedText": res.selectedText });
   });
-  console.log({ data });
 })();
