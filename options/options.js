29b525adb9b8a34c2f67752b81b0962b7863a578
Dusan <shandoo91@gmail.com>
2023-05-28 20:11:46 +0200

    updated async/await

--- options/options.js
+++ options/options.js
@@ -1,18 +1,24 @@
-(async () => {
+const data = async () => {
   const res = await chrome.storage.local.get('selectedText');
-  const clearStorageButton = document.querySelector('.clearStorageButton');
+  const selectedText = res.selectedText || [];
+  return selectedText;
+};
 
-  // clears the storage and all saved data
-  function clearStorage() {
-    if (window.confirm(`Are you sure you want to delete all saved data?`)) {
-      if (window.confirm(`All saved data will be deleted, press OK to continue`)) {
-        chrome.storage.local.clear();
-        chrome.runtime.sendMessage({ clearStorage: 'clear' });
-      }
-    } else {
-      return;
+const selectedText = await data();
+
+const clearStorageButton = document.querySelector('.clearStorageButton');
+
+// clears the storage and all saved data
+function clearStorage() {
+  if (window.confirm(`Are you sure you want to delete all saved data?`)) {
+    if (window.confirm(`All saved data will be deleted, press OK to continue`)) {
+      chrome.storage.local.clear();
+      chrome.runtime.sendMessage({ clearStorage: 'clear' });
     }
+  } else {
+    return;
   }
+}
 
-  clearStorageButton.addEventListener('click', clearStorage);
-})();
+clearStorageButton.addEventListener('click', clearStorage);
+
